f1f822198820cdf74079381ce92ec3d5e48fb206
Yusuf Abdullahi Temidayo <yusufabdullahi2910@gmail.com>
2024-05-23 00:04:34 +0100

    index.js

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+// components/index.js
+export { default as WalletCreation } from './WalletCreation';
+export { default as WalletTransfers } from './WalletTransfers';
+export { default as WalletRecovery } from './WalletRecovery';
+export { default as Notifications } from './Notifications';
+export { default as Contacts } from './Contacts';
