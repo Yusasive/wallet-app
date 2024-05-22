0a143132d4a8f2bc1d336503a4e6a1b82607f044
Yusuf Abdullahi Temidayo <yusufabdullahi2910@gmail.com>
2024-05-22 23:55:48 +0100

    App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// App.js
+import React from 'react';
+import { WalletCreation, WalletTransfers, WalletRecovery, Notifications, Contacts } from './components';
+
+function App() {
+  return (
+    <div>
+      <WalletCreation />
+      <WalletTransfers />
+      <WalletRecovery />
+      <Notifications />
+      <Contacts />
+    </div>
+  );
+}
+
+export default App;
